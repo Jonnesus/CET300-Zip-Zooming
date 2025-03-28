13e8edfe621b56f1730a579a13a514f594e9087c
Jonnesus <123877442+Jonnesus@users.noreply.github.com>
2025-03-28 21:36:53 +0000

    V0.2.0

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,7 +5,11 @@ public class InputManager : MonoBehaviour
 {
     [HideInInspector] public float move;
     [HideInInspector] public float steer;
+    [HideInInspector] public float handbrake;
+    [HideInInspector] public float boost;
 
     public void OnMove(InputValue value) => move = value.Get<Vector2>().y;
     public void OnSteer(InputValue value) => steer = value.Get<Vector2>().x;
+    public void OnHandbrake(InputValue value) => handbrake = value.Get<float>();
+    public void OnBoost(InputValue value) => boost = value.Get<float>();
 }
